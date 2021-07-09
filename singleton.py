4f964c707ab9083f5cfc35ea0afaefb48e0f0850
Vinícius da Rocha Motta <viniciusdarochamotta@gmail.com>
2021-07-09 18:59:13 -0300

    ultima

--- singleton.py
+++ singleton.py
@@ -3,6 +3,8 @@
 
 
 class Singleton(object):
+    """ Singleton class 
+    """
 
     def __new__(cls):
         """ Control for the instanciate objects
@@ -17,25 +19,30 @@ class Singleton(object):
         return cls.instance
 
 class LazySingleton(object):
+    """ Lazy instanciate singleton's object
+    """
 
-    __instance = None
+    __instance = None # initiate this atribute's value with None 
 
     def __init__(self) -> None:
+        print('tipo: ',type(self))
         if not LazySingleton.__instance:
             print('Method __init__ was called')
         else:
             print('One instance was already created', self.obter_instancia())
     
     @classmethod
-    def obter_instancia(cls):
+    def obter_instancia(cls) -> type(__instance): # this method belongs at this class else it can used without to create object, but using the name of this class 
         if not cls.__instance:
-            cls.__instance = Singleton()
+            cls.__instance = LazySingleton()
         return cls.__instance
 
 class MonoStateSingleton(object):
-    __estado_compartilhado = {'1': '2'}
+    """ MonoStateSingleton store state in all objects
+    """
+    __estado_compartilhado = {'1': '2'} # variable that holds state
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.x = 1
         # use __dict__ to store all states of one class
         self.__dict__ = self.__estado_compartilhado
@@ -51,12 +58,12 @@ if __name__ == '__main__':
     print(b1.__dict__)
     print(b2.__dict__)
 
-    # s1 = Singleton()
-    # print(s1)
-    # s2 = Singleton()
-    # print(s2)
-    # l1 = LazySingleton()
-    # LazySingleton.obter_instancia()
-    # print('Objeto criado: ', l1.obter_instancia())
-    # l2 = LazySingleton()
-    #print(l2)
+    s1 = Singleton()
+    print(s1)
+    s2 = Singleton()
+    print(s2)
+    l1 = LazySingleton()
+    LazySingleton.obter_instancia()
+    print('Objeto criado: ', l1.obter_instancia())
+    l2 = LazySingleton()
+    print(l2)
